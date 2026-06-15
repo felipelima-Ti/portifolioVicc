3e6c0594e61baa0f7ee52159a3d2bf07ed3cc990
unknown <felipelima1114@gmail.com>
2026-06-15 01:55:51 -0300

    update: background body

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -89,14 +89,14 @@ export default function Page() {
   </div>
 </div>
 
-            <p className="mt-50">Arquitetura & Design de Interiores</p>
+            <p className="mt-50 text-black/80">Arquitetura & Design de Interiores</p>
           </motion.h1>
 
           <motion.p
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 0.2, duration: 3 }}
-            className="mt-4 text-lg"
+            className="mt-4 text-lg text-black/70"
           >
             Venha transformar seus espaços conosco realizando seus sonhos!
           </motion.p>
@@ -108,7 +108,6 @@ export default function Page() {
           id="sobre"
           className=""
         >
-           <hr className="border-[#dafdc6] mb-20"></hr>
           <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center gap-16 mt-24 px-4">
           <div className="flex-1">
             <motion.h2
@@ -116,29 +115,32 @@ export default function Page() {
               initial="hidden"
               whileInView="show"
               viewport={{ once: true }}
-              className="text-3xl font-semibold mb-6 text-gray-700"
+              className="text-3xl font-semibold mb-6 text-black/80"
             >
-              Sobre Mim
+              Sobre o Estudio
             </motion.h2>
+            <hr className="border-1 border-[#769068] mb-10 w-80"></hr>
 
             <motion.p
               variants={fadeSlide}
               initial="hidden"
               whileInView="show"
               viewport={{ once: true }}
-              className="text-gray-700 leading-relaxed mb-6"
+              className="text-black  leading-relaxed mb-6"
             >
-              <b>Olá, sou Victoria Tenfen</b>
+              <b className="text -xm text-green-900 text-2xl">Olá, meu nome e Victoria Tenfen</b>
               <br />
+              <p className="">
               Sou apaixonada por transformar espaços em experiências. Acredito que cada
               ambiente carrega uma história — e meu trabalho é revelar essa narrativa
               através de equilíbrio, função e estética. Atuo na área de Arquitetura e
               Design de Interiores, criando projetos que unem conforto, personalidade e
               propósito.
+              </p>
             </motion.p>
 
             <Link href="#contato">
-              <button className="mt-10 h-10 bg-[#769068] w-60 rounded-full text-white">
+              <button className="mt-10 h-10 bg-[#486e37]/89 w-60 rounded-xl text-white">
                 Mande-me uma mensagem
               </button>
             </Link>
@@ -156,7 +158,7 @@ export default function Page() {
               alt="Foto de Perfil"
               width={350}
               height={350}
-              className="p-2 rounded-full shadow-lg border border-gray-600"
+              className="p-2 rounded-full shadow-lg border border-[#486e37]"
             />
           </motion.div>
           </div>
@@ -178,7 +180,7 @@ export default function Page() {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 justify-start">
               {/* Bloco 1 */}
               <div className="flex flex-col items-start">
-                <p className="text-xl mb-2">Comercial ivapora</p>
+                <p className="text-xl mb-2 ">Comercial ivapora</p>
                 <Image
                   src="/comercio.jpeg"
                   alt="Foto de Perfil"
@@ -186,8 +188,8 @@ export default function Page() {
                   height={300}
                   className="rounded-xl shadow-lg"
                 />
-                <p className="text-sm mt-2">Design de interiores</p>
-                <p className="text-sm mt-2"><b>1 ano e 3 mes</b></p>
+                <p className="text-sm mt-2 ">Design de interiores</p>
+                <p className="text-sm mt-2 text-[#486e37]"><b>1 ano e 3 mes</b></p>
               </div>
 
               {/* Bloco 2 */}
@@ -201,12 +203,12 @@ export default function Page() {
                   className="rounded-xl shadow-lg"
                 />
                 <p className="text-sm mt-2">Comprador - Obras</p>
-                <p className="text-sm mt-2"><b>2 ano e 1 mes</b></p>
+                <p className="text-sm mt-2 text-[#486e37]"><b>2 ano e 1 mes</b></p>
               </div>
 
               {/* Bloco 3 */}
               <div className="flex flex-col items-start">
-                <p className="text-xl mb-2">JD Vidros e Esquadrias</p>
+                <p className="text-xl mb-2 ">JD Vidros e Esquadrias</p>
                 <Image
                   src="/jd.jpeg"
                   alt="Foto de Perfil"
@@ -215,7 +217,7 @@ export default function Page() {
                   className="rounded-xl shadow-lg"
                 />
                 <p className="text-sm mt-2">Projetista CAD</p>
-                <p className="text-sm mt-2"><b>5 meses</b></p>
+                <p className="text-sm mt-2 text-[#486e37]"><b>5 meses</b></p>
               </div>
 
               {/* Bloco 4 */}
@@ -229,7 +231,7 @@ export default function Page() {
                   className="rounded-xl shadow-lg"
                 />
                 <p className="text-sm mt-2">Projetista</p>
-                <p className="text-sm mt-2"><b>2 ano e 8 mes</b></p>
+                <p className="text-sm mt-2 text-[#486e37]"><b>2 ano e 8 mes</b></p>
               </div>
             </div>
           </div>
@@ -237,7 +239,7 @@ export default function Page() {
         <section id="services" className="ml-4 mr-5 mt-20"></section>
        <div className=" py-20">
   <div className="text-center mb-16">
-    <h2 className="text-3xl font-bold text-black">
+    <h2 className="text-3xl font-bold text-green-900/80 ">
       Conheça nossos serviços
     </h2>
     <p className="mt-5 text-gray-600">
@@ -250,79 +252,79 @@ export default function Page() {
   <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 px-6 ">
     
     {/* Card */}
-    <div className="bg-[#cbf2c2] p-10">
-      <div className="w-12 h-12 bg-[#bbf2a2] flex items-center justify-center mb-6">
+    <div className="bg-[#769068]/97 p-10 rounded-xl">
+      <div className="w-12 h-12 bg-[#769068]/50 text-white flex items-center justify-center mb-6">
         <Home />
       </div>
-      <h3 className="text-xl font-semibold mb-4">
+      <h3 className="text-xl text-white font-semibold mb-4">
         Arquitetura Residencial
       </h3>
-      <p className="text-gray-600 leading-relaxed">
+      <p className="text-white leading-relaxed">
         Projetos de casas e apartamentos que combinam conforto, funcionalidade e
         estética personalizada.
       </p>
     </div>
 
-    <div className="bg-[#cbf2c2] p-10">
-      <div className="w-12 h-12 bg-[#bbf2a2] flex items-center justify-center mb-6">
+    <div className="bg-[#769068]/97 p-10 rounded-xl">
+      <div className="w-12 h-12 bg-[#769068]/50 text-white flex items-center justify-center mb-6">
         <Building2 />
       </div>
-      <h3 className="text-xl font-semibold mb-4">
+      <h3 className="text-xl text-white font-semibold mb-4">
         Arquitetura Comercial
       </h3>
-      <p className="text-gray-600 leading-relaxed">
+      <p className="text-gray-600 leading-relaxed text-white">
         Espaços corporativos e comerciais que otimizam a experiência de trabalho
         e fortalecem marcas.
       </p>
     </div>
 
-    <div className="bg-[#cbf2c2] p-10">
-      <div className="w-12 h-12 bg-[#bbf2a2] flex items-center justify-center mb-6">
+    <div className="bg-[#769068]/97 p-10 rounded-xl">
+      <div className="w-12 h-12 bg-[#769068]/50 text-white flex items-center justify-center mb-6">
         <Palette />
       </div>
-      <h3 className="text-xl font-semibold mb-4">
+      <h3 className="text-xl font-semibold mb-4 text-white">
         Design de Interiores
       </h3>
-      <p className="text-gray-600 leading-relaxed">
+      <p className="text-gray-600 leading-relaxed text-white">
         Transformação de ambientes internos com seleção de materiais,
         mobiliário e iluminação.
       </p>
     </div>
 
-    <div className="bg-[#cbf2c2] p-10">
-      <div className="w-12 h-12 bg-[#bbf2a2] flex items-center justify-center mb-6">
+    <div className="bg-[#769068]/97 p-10 rounded-xl">
+      <div className="w-12 h-12 bg-[#769068]/50 text-white flex items-center justify-center mb-6">
         <Lightbulb />
       </div>
-      <h3 className="text-xl font-semibold mb-4">
+      <h3 className="text-xl text-white font-semibold mb-4">
         Lighting Design
       </h3>
-      <p className="text-gray-600 leading-relaxed">
+      <p className="text-gray-600 leading-relaxed text-white">
         Projetos de iluminação que valorizam a arquitetura e criam atmosferas
         únicas.
       </p>
     </div>
 
-    <div className="bg-[#cbf2c2] p-10">
-      <div className="w-12 h-12 bg-[#bbf2a2] flex items-center justify-center mb-6">
+    <div className="bg-[#769068]/97 p-10 rounded-xl">
+      <div className="w-12 h-12 bg-[#769068]/50 text-white flex items-center justify-center mb-6">
         <Trees />
       </div>
-      <h3 className="text-xl font-semibold mb-4">
+      <h3 className="text-xl font-semibold mb-4 text-white">
         Paisagismo
       </h3>
-      <p className="text-gray-600 leading-relaxed">
+      <p className="text-gray-600 leading-relaxed text-white">
         Integração harmoniosa entre arquitetura e natureza através de projetos de
         áreas externas.
       </p>
     </div>
 
-    <div className="bg-[#cbf2c2] p-10">
-      <div className="w-12 h-12 bg-[#bbf2a2] flex items-center justify-center mb-6">
+    <div className="bg-[#769068]/97 p-10 rounded-xl">
+      <div className="w-12 h-12 bg-[#769068]/50 text-white flex items-center justify-center mb-6">
         <Ruler />
       </div>
-      <h3 className="text-xl font-semibold mb-4">
+      <h3 className="text-xl font-semibold mb-4 text-white">
         Reforma & Retrofit
       </h3>
-      <p className="text-gray-600 leading-relaxed">
+      <p className="text-gray-600 leading-relaxed text-white">
         Renovação de espaços existentes mantendo a essência e agregando
         modernidade.
       </p>
@@ -415,11 +417,11 @@ export default function Page() {
         </section>
         <section id="contato" className="max-w-6xl mx-auto mt-12 ">
           <hr className="mb-20 border-[#dafdc6] w-100 "></hr>
-          <h2 className="text-3xl font-semibold text-gray-700 ml-5 "><b> Contato</b></h2>
-          <p className="font-semibold mb-1 text-gray-700 ml-5">Entre em contato para transformar seus espaços!</p>
+           <p className=" text-2xl font-semibold mb-1 text-black/70  mb-10 ">Contato</p>
+          <p className=" font-semibold mb-1 text-black/70  mb-10 ">Entre em contato para transformar seus espaços!</p>
          <div className="flex gap-10 mt-1 text-3xl ml-20">
 </div>
-<div className="mb-40 bg-[] p-5 rounded-xl w-full">
+<div className="mb-40 border border-green-700/60 p-5 rounded-xl w-full bg-[#769068]/10">
      <div className="flex mb-12">
             <a href="https://www.linkedin.com/in/vit%C3%B3ria-tenfen-25575622a/" className="text-blue-700 hover:underline">Linkedin
               <FaLinkedin size={24} className="text-blue-700" />
@@ -443,7 +445,7 @@ export default function Page() {
   <input
     name="name"
     placeholder="Seu Nome"
-    className="w-full p-3 rounded-xl bg-white/5 border border-black/40"
+    className="w-full p-3 rounded-xl bg-[#769068]/30 border border-green-700/60"
     required
   />
 
@@ -451,7 +453,7 @@ export default function Page() {
     type="email"
     name="email"
     placeholder="Seu Email"
-    className="w-full p-3 rounded-xl bg-white/5 border border-black/40"
+    className="w-full p-3 rounded-xl bg-[#769068]/30 border border-green-700/60"
     required
   />
 
@@ -459,7 +461,7 @@ export default function Page() {
     name="message"
     placeholder="Sua Mensagem"
     rows="4"
-    className="w-full p-3 rounded-xl bg-white/5 border border-black/40"
+    className="w-full p-3 rounded-xl bg-[#769068]/30 border border-green-700/60"
     required
   />
   <input type="hidden" name="_subject" value="Nova mensagem no seu site!" />
